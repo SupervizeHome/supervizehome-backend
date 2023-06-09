822b4288724b5f0dcbba096670156508d50c8a50
ChalalSamir <samir-94@outlook.com>
2023-06-09 20:47:31 +0200

    add cloudinary

---
+++ config/plugin.js
@@ -0,0 +1,12 @@
+module.exports = ({ env }) => ({
+    // Autres configurations de plugins existants...
+  
+    upload: {
+      provider: 'cloudinary',
+      providerOptions: {
+        cloud_name: 'dizcasdok',
+        api_key: '625211673148671',
+        api_secret: 'gGSsqCOggeTRaR-SFlqK5DQzYMY',
+      },
+    },
+  });
